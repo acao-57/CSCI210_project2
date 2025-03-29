191652cd034271c92af071ab0229d3ae509844e9
acao-57 <amycao78@gmail.com>
2025-03-30 03:48:47 +0800

    Update mkdirSplitpath.c

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -41,7 +41,7 @@ void mkdir(char pathName[]){
     {
         if (strcmp(child->name, baseName) == 0 && child->fileType == 'D') //checks if the target directory already exists in current directory
         {
-            printf("MKDIR ERROR: directory %s already exists\n", dirName);
+            printf("MKDIR ERROR: directory %s already exists\n", pathName);
             return;
         }
         child = child->siblingPtr;  //cycles thru all siblings
